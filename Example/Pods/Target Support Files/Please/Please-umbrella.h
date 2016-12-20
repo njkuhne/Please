f0c237f0e7b2d6948a70271c00ee85aac5d4548c
Nicholas Kuhne <njkuhne@me.com>
2016-12-20 11:42:01 -0500

    Initial pod lib create

---
+++ Example/Pods/Target Support Files/Please/Please-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double PleaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char PleaseVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-Please_Example/Pods-Please_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Please_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Please_ExampleVersionString[];
+
